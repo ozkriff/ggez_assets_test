34f3340fd59d64e27fae68ca560e6219aecf25bb
Andrey Lesnikov <ozkriff@gmail.com>
2018-10-05 19:50:17 +0300

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,53 @@
 extern crate ggez;
- 
-use ggez::graphics::{Point2, DrawParam, draw_ex};
-use ggez::graphics;
-use ggez::conf;
-use ggez::{Context, ContextBuilder, GameResult};
-use ggez::event::EventHandler;
-use ggez::timer;
-use ggez::event;
- 
+
+use ggez::{
+    conf, event,
+    event::EventHandler,
+    graphics, {Context, ContextBuilder, GameResult},
+};
+
 struct Assets {
-    player_image: graphics::Image
+    player_image: graphics::Image,
 }
+
 impl Assets {
     fn new(ctx: &mut Context) -> GameResult<Assets> {
-        let player_image = graphics::Image::new(ctx, "./ player.png")?;
- 
-        Ok(Assets {
-            player_image,
-        })
+        let player_image = graphics::Image::new(ctx, "/player.png")?;
+        Ok(Assets { player_image })
     }
 }
- 
+
 struct MainState {
     assets: Assets,
 }
+
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let assets = Assets::new(ctx)?;
- 
-        let s = MainState {
-            assets,
-        };
- 
-        Ok(s)
+        Ok(MainState { assets })
     }
-}
- 
-impl EventHandler for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        const DESIRED_FPS: u32 = 60;
-        while timer::check_update_time(ctx, DESIRED_FPS) {
-            let seconds = 1.0 / (DESIRED_FPS as f32);
-        }
-   
-        Ok(())
-    }
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
- 
-        let assets = &mut self.assets;
- 
-        Ok(())
-    }
-}
- 
-fn main() {
-    let mut cb = ContextBuilder::new("Game", "ggez")
-        .window_setup(conf::WindowSetup::default().title("Game Title"))
-        .window_mode(conf::WindowMode::default().dimensions(700, 700));
- 
-    let ctx = &mut cb.build().unwrap();
-    let state = &mut MainState::new(ctx).unwrap();
- 
-    event::run(ctx, state);
- 
 }
 
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        let dst = graphics::Point2::new(20.0, 20.0);
+        graphics::draw(ctx, &self.assets.player_image, dst, 0.0)?;
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+fn main() {
+    let cb = ContextBuilder::new("Game", "ggez")
+        .add_resource_path("resources")
+        .window_setup(conf::WindowSetup::default().title("Game Title"))
+        .window_mode(conf::WindowMode::default().dimensions(700, 700));
+    let ctx = cb.build().unwrap();
+    let state = MainState::new(&mut ctx).unwrap();
+    event::run(&mut ctx, &mut state).unwrap();
+}
